249af91edf4ed44a4d59fe3a6e2a01cb261fa393
PriyaYen99 <Priyayen99@gmail.com>
2024-06-10 12:42:38 -0700

    zipfileremoved

---
+++ FizzBuzzApp/FizzBuzzApp/FizzBuzzApp/FizzBuzz.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzBuzzApp
+{
+    public class FizzBuzz
+    {
+        public static List<string> ProcessFizzBuzz(List<string> inputs)
+        {
+            List<string> results = new List<string>();
+
+            foreach (var input in inputs)
+            {
+                if (int.TryParse(input, out int number))
+                {
+                    if (number % 3 == 0 && number % 5 == 0)
+                    {
+                        results.Add("FizzBuzz");
+                    }
+                    else if (number % 3 == 0)
+                    {
+                        results.Add("Fizz");
+                    }
+                    else if (number % 5 == 0)
+                    {
+                        results.Add("Buzz");
+                    }
+                    else
+                    {
+                        results.Add($"Divided {number} by 3");
+                        results.Add($"Divided {number} by 5");
+                    }
+                }
+                else
+                {
+                    results.Add("Invalid item");
+                }
+            }
+
+            return results;
+        }
+
+        public static void DisplayResults(List<string> results)
+        {
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ FizzBuzzApp/FizzBuzzApp/FizzBuzzApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzBuzzApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputs = new List<string> { "1", "3", "5", "", "15", "A", "23" };
+            List<string> results = FizzBuzz.ProcessFizzBuzz(inputs);
+            FizzBuzz.DisplayResults(results);
+        }
+    }
+}
